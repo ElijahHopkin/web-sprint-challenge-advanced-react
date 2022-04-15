af10e757dd137720857bbc7848c3340b6d4e0a05
enhopkin@gmail.com <enhopkin@gmail.com>
2022-04-15 12:24:32 -0600

    mvp complete round 2

--- frontend/components/App.test.js
+++ frontend/components/App.test.js
@@ -1,9 +1,22 @@
 // Write your tests here
 import React from 'react'
-import {screen, render} from '@testing-library/react'
+import {screen, render, fireEvent} from '@testing-library/react'
 import AppClass from './AppClass'
+import AppFunctional from './AppFunctional'
 
-test('AppClass responds to directional buttons', () => {
+test('AppClass renders with no errors', () => {
   render(<AppClass />)
 })
 
+test('AppFunctional renders with no errors', () => {
+  render(<AppFunctional />)
+})
+
+test('Functional link transitions from AppClass to AppFunctional', async () => {
+  // render(<AppClass />);
+  // const functional = document.querySelector('Functional')
+  // fireEvent.click(functional);
+  // expect (<AppFunctional />).toBeInT()
+  // const appFunctional = screen.findBy
+
+})
